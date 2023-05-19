0937dd39cfbee2661b48d82a854cead092621376
august passov <gus.passov@kellogg.northwestern.edu>
2023-05-19 00:15:46 +0000

    Class 8 Exercises

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,11 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "Halfmoon15"
+encrypted_secret = BCrypt::Password.create(secret)
+puts encrypted_secret
 # 2. prepare encrypted string for testing
-# 3. test secret against prepared encrypted string
+user_entered_secret = "Halfmoon15"
+result = BCrypt::Password.new(encrypted_secret) == user_entered_secret 
+# 3. test secret against prepared encrypted 
+puts result
